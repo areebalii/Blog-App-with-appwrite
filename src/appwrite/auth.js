946bda489c43e15bea734f0cee265d70beec886d
areebalii <areebktk8@gmail.com>
2025-07-04 11:05:32 +0500

    set up project

---
+++ src/appwrite/auth.js
@@ -0,0 +1,19 @@
+import conf from "../conf/conf.js";
+import { Client, Account, ID } from "appwrite";
+
+
+export class AuthService {
+  client = new Client();
+  account;
+
+  constructor() {
+    this.client
+      .setEndpoint(conf.endPoint) 
+      .setProject(conf.projectId);
+  }
+  
+} 
+
+const authService = new AuthService();
+
+export default AuthService
